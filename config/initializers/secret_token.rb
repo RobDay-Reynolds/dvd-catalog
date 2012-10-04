d34b34f26181ab17a005d46730836bad3f899ab7
Rob Day-Reynolds <rob.dayreynolds@gmail.com>
2012-10-03 19:19:35 -0700

    Initial commit.  Empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DvdCatalog::Application.config.secret_token = '12a5e3a3c9dda28ad91ffa4b9646c8c4051502d7d89145007c1794e39126df081d0483647be7e5ba5e8ebc97016b90632f2dec078dbca45e0535d5e8573e0bf3'
